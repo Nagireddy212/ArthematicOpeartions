fef82c418777b921c56761f46584c94c33f87e4d
unknown <somulanagireddy123@gmail.com>
2021-02-22 15:39:12 +0530

    changed UC1.sh

--- UC1.sh
+++ UC1.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 declare -A dict
+declare -a array
 read -p " Enter the number1" a
 read -p "Enter the number2" b
 read -p "Enter the number3" c
 
 result1=$(( $a+$b*$c ))
-
 result2=$(( $a*$b+$c ))
-
 result3=$(( $c+$a/$b ))
-
 result4=(( $a%$b+$c ))
 
 dict=([1]=$result [2]=$result2 [3]=$result3 [4]$result4)
 
 echo "a+b*c : $result1"
-
 echo "a*b+c : $result2"
-
 echo "c+a/b : $result3"
-
 echo "a%b+c : $result4"
-
 echo "Array Elements : ${dict[@]}"
+
+echo "Dictionary value : "${dict[@]}
+for (( counter=1; counter<=4; counter++ ))
+do
+	array[$counter]=${dict[$counter]}
+done
+echo "Array values : ${array[@]}"
